e8d2aab9238a1051ea6c66494dcd7bb115e3560f
Oscar Jernemalm <oscarj@n152-p89.eduroam.kth.se>
2026-04-22 09:59:53 +0200

    Initial commit

---
+++ bikeshop/src/main/java/se/kth/iv1350/dto/CustomerDTO.java
@@ -0,0 +1,24 @@
+package se.kth.iv1350.dto;
+
+public class CustomerDTO {
+
+    private String phoneNumber;
+    private String name;
+
+    public CustomerDTO(String phoneNumber, String name){
+        this.phoneNumber = phoneNumber;
+        this.name = name;
+    }
+
+    //getters
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+    public String getName(){
+        return name;
+    }
+
+    
+
+
+}
