7834d533b90acb8c9818fe629e019f6d4d6a58a0
Gustaf Blomquist <glustone@proton.me>
2026-04-29 14:43:13 +0200

    Uppdatering test

--- src/test/java/se/kth/iv1350/model/RepairOrderTest.java
+++ src/test/java/se/kth/iv1350/model/RepairOrderTest.java
@@ -10,55 +10,75 @@ import se.kth.iv1350.integration.RepairOrderDTO;
 import se.kth.iv1350.integration.RepairOrderRegistry;
 
 public class RepairOrderTest {
-    private String problemDescr = "broken brakes"; 
-    private String phoneNumber = "123456789"; 
-    private String bikeSerialNumber = "BIKE123"; 
+    private String problemDescr; 
+    private String phoneNumber; 
+    private String bikeSerialNumber; 
     private RepairOrderRegistry repairOrderRegistry;
     private RepairOrder testRepairOrder;
+    private String diagTaskResult;
     private String repairOrderId;
-    private String diagTaskResult = "broken brakes indeed";
+    private RepairTask repairTask;
+    private String repairTaskDescription;
+    private double cost;
 
-    @Test
 
+    private void createTestRepairTask(){
+    repairTaskDescription = "replace brake wire";
+            cost = 125;
+            repairTask = RepairTask.createRepairTask(repairTaskDescription, cost);
+    
+    }
 
     @Before
         public void setUp() {
+            problemDescr = "broken brakes"; 
+            phoneNumber = "123456789"; 
+            bikeSerialNumber = "BIKE123"; 
+            diagTaskResult = "broken brakes indeed";
             repairOrderRegistry= new RepairOrderRegistry();
             testRepairOrder = RepairOrder.createRepairOrder(problemDescr, phoneNumber, bikeSerialNumber, repairOrderRegistry);
-            testRepairOrder.addDiagnosticReport(diagTaskResult);
+            repairOrderId = repairOrderRegistry.generateRepairOrderId();
+            createTestRepairTask();
     }
     
     @After
         public void tearDown() {
+            problemDescr = "broken brakes"; 
+            phoneNumber = "123456789"; 
+            bikeSerialNumber = "BIKE123"; 
+            diagTaskResult = null;
             repairOrderRegistry = null;
             testRepairOrder = null;
             repairOrderId = null;
-            diagTaskResult = null;
-}
+    }
     @Test
     public void  testCreateRepairOrderWithCorrectValues(){
-        
-        String repairOrderId = repairOrderRegistry.generateRepairOrderId();
 
-        assertNotNull(testRepairOrder);
-        assertNotNull(testRepairOrder.getRepairOrderId());
-        assertEquals("broken brakes", testRepairOrder.getProblemDescr());
-        assertEquals("123456789", testRepairOrder.getPhoneNumber());
-        assertEquals("BIKE123", testRepairOrder.getBikeSerialNumber());
+        assertNotNull("There is no RepairOrder",testRepairOrder); //kör fler messages
+        assertNotNull("There is no RepairOrderId",testRepairOrder.getRepairOrderId());
+        assertEquals("Not the same RepairOrderId",repairOrderId, repairOrderRegistry.generateRepairOrderId());
+        assertEquals("Wrong problemDescr","broken brakes", testRepairOrder.getProblemDescr());
+        assertEquals("Wrong phoneNumber","123456789", testRepairOrder.getPhoneNumber());
+        assertEquals("Wrong BikeSerialNumber","BIKE123", testRepairOrder.getBikeSerialNumber());
 
     }
+    
 
     @Test
     public void testAddDiagnosticReportWithCorrectValues(){
-        assertNotNull(testRepairOrder.getDiagnosticReport());
-        assertEquals("broken brakes indeed", testRepairOrder.getDiagnosticReport());
+        testRepairOrder.addDiagnosticReport(diagTaskResult);
+        assertNotNull("There is no Diagnostic Report", testRepairOrder.getDiagnosticReport());
+        assertEquals("Diagnotic Report not the same", "broken brakes indeed", testRepairOrder.getDiagnosticReport());
 
     }
 
+
     @Test public void testAddRepairTasktWithCorrectValues(){
+        testRepairOrder.addRepairTask(repairTask);
+        assertNotNull("There is no repairTask added", .getRepairTask());        //behöver göra getRepairTask
+        assertEquals("Diagnotic Report not the same", "broken brakes indeed", testRepairOrder.getDiagnosticReport());
 
-
-
+    
 
     }
    public void addRepairTask(RepairTask repairTask){
@@ -67,6 +87,32 @@ public class RepairOrderTest {
             nrOfRepairTasks++;
             totalCost = calculateTotalCost(repairTasks);
         }
+    }
+
+    @Test 
+    public void testCalculateTotalCostBeforeAddRepairTask(){
+        
+        assertNull(testRepairOrder.calculateTotalCost());
+    
+    }
+
+     @Test 
+    public void testCalculateTotalCostAfterAddRepairTask(){
+        
+        assertNotNull(testRepairOrder.calculateTotalCost());
+    
+    }
+
+
+     public double calculateTotalCost(RepairTask[] repairTasks){
+        double sum = 0;
+
+        for(int i = 0; i < repairTasks.length; i++){
+            if(repairTasks[i] != null){
+                sum += repairTasks[i].getCost();
+            }
+        }
+        return sum;
     }
 }
 
