58efe98e4323afe67994320386cfae7ab40ef83d
Gustaf Blomquist <glustone@proton.me>
2026-04-29 15:38:22 +0200

    Finished repairOrderTest

--- src/test/java/se/kth/iv1350/model/RepairOrderTest.java
+++ src/test/java/se/kth/iv1350/model/RepairOrderTest.java
@@ -18,14 +18,15 @@ public class RepairOrderTest {
     private String diagTaskResult;
     private String repairOrderId;
     private RepairTask repairTask;
+    private RepairTask [] repairTasks;
     private String repairTaskDescription;
     private double cost;
-
+    private RepairOrderDTO testDto;
 
     private void createTestRepairTask(){
-    repairTaskDescription = "replace brake wire";
-            cost = 125;
-            repairTask = RepairTask.createRepairTask(repairTaskDescription, cost);
+        repairTaskDescription = "replace brake wire";
+        cost = 125;
+        repairTask = RepairTask.createRepairTask(repairTaskDescription, cost);
     
     }
 
@@ -39,6 +40,7 @@ public class RepairOrderTest {
             testRepairOrder = RepairOrder.createRepairOrder(problemDescr, phoneNumber, bikeSerialNumber, repairOrderRegistry);
             repairOrderId = repairOrderRegistry.generateRepairOrderId();
             createTestRepairTask();
+            repairTasks = new RepairTask[10];
     }
     
     @After
@@ -50,70 +52,76 @@ public class RepairOrderTest {
             repairOrderRegistry = null;
             testRepairOrder = null;
             repairOrderId = null;
+            repairTask = null;
+            repairTasks = null;
+            testDto = null;
     }
+
     @Test
     public void  testCreateRepairOrderWithCorrectValues(){
-
         assertNotNull("There is no RepairOrder",testRepairOrder); //kör fler messages
         assertNotNull("There is no RepairOrderId",testRepairOrder.getRepairOrderId());
         assertEquals("Not the same RepairOrderId",repairOrderId, repairOrderRegistry.generateRepairOrderId());
         assertEquals("Wrong problemDescr","broken brakes", testRepairOrder.getProblemDescr());
         assertEquals("Wrong phoneNumber","123456789", testRepairOrder.getPhoneNumber());
         assertEquals("Wrong BikeSerialNumber","BIKE123", testRepairOrder.getBikeSerialNumber());
-
     }
-    
 
     @Test
     public void testAddDiagnosticReportWithCorrectValues(){
         testRepairOrder.addDiagnosticReport(diagTaskResult);
         assertNotNull("There is no Diagnostic Report", testRepairOrder.getDiagnosticReport());
         assertEquals("Diagnotic Report not the same", "broken brakes indeed", testRepairOrder.getDiagnosticReport());
-
     }
-
 
     @Test public void testAddRepairTasktWithCorrectValues(){
         testRepairOrder.addRepairTask(repairTask);
-        assertNotNull("There is no repairTask added", .getRepairTask());        //behöver göra getRepairTask
-        assertEquals("Diagnotic Report not the same", "broken brakes indeed", testRepairOrder.getDiagnosticReport());
 
-    
-
-    }
-   public void addRepairTask(RepairTask repairTask){
-        if(nrOfRepairTasks < repairTasks.length){
-            repairTasks[nrOfRepairTasks] = repairTask;
-            nrOfRepairTasks++;
-            totalCost = calculateTotalCost(repairTasks);
-        }
+        assertNotNull("There is no repairTask added", testRepairOrder.getRepairTasks());
+        repairTasks [0] = repairTask;
+        assertArrayEquals("RepairTask is not the same", repairTasks, testRepairOrder.getRepairTasks());
     }
 
     @Test 
-    public void testCalculateTotalCostBeforeAddRepairTask(){
-        
-        assertNull(testRepairOrder.calculateTotalCost());
-    
+    public void testCalculateTotalCost(){
+        assertEquals("Total cost is not zero when repairTasks is empty",0.0, testRepairOrder.calculateTotalCost(repairTasks), 0.01);
+
+        testRepairOrder.addRepairTask(repairTask);
+        repairTasks [0] = repairTask;
+
+        assertNotEquals("Totalcost is zero after adding repairTask", 0.0, testRepairOrder.calculateTotalCost(repairTasks));
+        repairTasks [1] = repairTask;
+        assertEquals("Totalcost is not 250",250,testRepairOrder.calculateTotalCost(repairTasks),0.01);
     }
 
-     @Test 
-    public void testCalculateTotalCostAfterAddRepairTask(){
-        
-        assertNotNull(testRepairOrder.calculateTotalCost());
-    
+    @Test
+    public void testUpdateRepairOrderStatus(){
+        assertEquals("Its not CREATED to begin with","CREATED", testRepairOrder.getState());
+
+        testRepairOrder.updateRepairOrderStatus("REJECTED");
+
+        assertEquals("Its not REJECTED","REJECTED", testRepairOrder.getState());
     }
 
+    @Test
+    public void testCreateRepairOrderDTO(){
+        testRepairOrder.addDiagnosticReport(diagTaskResult);
+        testRepairOrder.addRepairTask(repairTask);
 
-     public double calculateTotalCost(RepairTask[] repairTasks){
-        double sum = 0;
+        testDto = testRepairOrder.createRepairOrderDTO();
 
-        for(int i = 0; i < repairTasks.length; i++){
-            if(repairTasks[i] != null){
-                sum += repairTasks[i].getCost();
-            }
-        }
-        return sum;
+        assertEquals("RepairOrderId is not the same",testRepairOrder.getRepairOrderId(), testDto.getRepairOrderId());
+        assertEquals("problemDescr is not the same",testRepairOrder.getProblemDescr(), testDto.getProblemDescr());
+        assertEquals("phoneNumber is not the same",testRepairOrder.getBikeSerialNumber(), testDto.getBikeSerialNumber());
+        assertEquals("bikeSerialNumber is not the same ",testRepairOrder.getProblemDescr(), testDto.getProblemDescr());
+        assertEquals("state is not the same",testRepairOrder.getState(), testDto.getState());
+        assertEquals("totalCost is not the same",testRepairOrder.getTotalCost(), testDto.getTotalCost(), 0.01);
+        assertEquals("diagnosticReport is not the same",testRepairOrder.getDiagnosticReport(), testDto.getDiagnosticReport());
+        assertEquals("estimatedCompletionDate is not the same",testRepairOrder.getEstimatedCompletionDate(), testDto.getEstimatedCompletionDate());
+        assertEquals("nrOfRepairTasks is not the same",testRepairOrder.getNrOfRepairTasks(), testDto.getNrOfRepairTasks());
+        assertArrayEquals("repairTasks is not the same",testRepairOrder.getRepairTasks(), testDto.getRepairTasks());
     }
+
 }
 
 
