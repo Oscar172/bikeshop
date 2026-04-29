9a552a6471f1e1c53b55cf0ed124dcccafe0f932
Gustaf Blomquist <glustone@proton.me>
2026-04-29 12:15:25 +0200

    Changed createRepairOrder to return object in order to test it

--- src/main/java/se/kth/iv1350/model/RepairOrder.java
+++ src/main/java/se/kth/iv1350/model/RepairOrder.java
@@ -47,10 +47,12 @@ public class RepairOrder {
      * @param bikeSerialNumber The bike's serial number.
      * @param repairOrderRegistry The registry where the repair order is stored.
      */
-    public static void createRepairOrder(String problemDescr, String phoneNumber, String bikeSerialNumber, RepairOrderRegistry repairOrderRegistry){
+    public static RepairOrder createRepairOrder(String problemDescr, String phoneNumber, String bikeSerialNumber, RepairOrderRegistry repairOrderRegistry){
         String repairOrderId = repairOrderRegistry.generateRepairOrderId();
         RepairOrder repairOrder = new RepairOrder(repairOrderId, problemDescr, phoneNumber, bikeSerialNumber, repairOrderRegistry);
         repairOrderRegistry.updateRepairOrder(repairOrder);
+
+        return repairOrder;
     }
 
     /**
