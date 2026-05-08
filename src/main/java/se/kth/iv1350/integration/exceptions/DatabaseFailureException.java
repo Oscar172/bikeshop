48bb724938234960f3b70e8ddfa3e119885b4a50
Tova Norlykke <tova.norlykke@gmail.com>
2026-05-08 12:08:36 +0200

    added comments to exception classes

--- src/main/java/se/kth/iv1350/integration/exceptions/DatabaseFailureException.java
+++ src/main/java/se/kth/iv1350/integration/exceptions/DatabaseFailureException.java
@@ -1,6 +1,16 @@
 package se.kth.iv1350.integration.exceptions;
 
+/**
+ * Thrown when a database operation fails because of technical issues,
+ * for instance a lost connection.
+ * This is an unchecked exception, it represents an unpredictable system error
+ * that is not expected to be handled in the basic flow of the application.
+ */
 public class DatabaseFailureException extends RuntimeException {
+    /**
+     * Creates a new instance with a message that describe the error.
+     * @param message A message explaining what went wrong.
+     */
     public DatabaseFailureException (String message) {
         super(message);
     }
--- src/main/java/se/kth/iv1350/integration/exceptions/UserNotFoundException.java
+++ src/main/java/se/kth/iv1350/integration/exceptions/UserNotFoundException.java
@@ -1,6 +1,16 @@
 package se.kth.iv1350.integration.exceptions;
 
+/**
+ * Thrown when the search for a secific customer fails because
+ * the provided phone number does not exist in the customer regisrty.
+ * This is a checked exception as it represents a predictable error that
+ * the application is expected to handle.
+ */
 public class UserNotFoundException extends Exception {
+    /**
+     * Creates a new instance with a message that describe the error.
+     * @param message A message explaining what went wrong.
+     */
     public UserNotFoundException(String message) {
         super(message);
     }
