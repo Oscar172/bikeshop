fed1287ff74f219f255bc2624eced1e0612e6799
Tova Norlykke <tova.norlykke@gmail.com>
2026-05-08 11:57:27 +0200

    fix on try catch i view

--- src/main/java/se/kth/iv1350/view/View.java
+++ src/main/java/se/kth/iv1350/view/View.java
@@ -25,40 +25,38 @@ public class View {
      * Simulates a user input that generates calls to system operations.
      */
     public void runFakeExecution() {
-        String phoneNumber = "1223";
+        String phoneNumber = "0000";
         CustomerDTO foundCustomer = null;
         try {
             foundCustomer = contr.searchForCustomer(phoneNumber);
+
+            System.out.println("----- Sample Execution -----");
+            System.out.println("Customer found: " + foundCustomer.getName());
+
+            contr.createRepairOrder("Flat tire and a broken front-light", phoneNumber, "BIKE-001");
+
+            String repairOrderId = "RO-1";
+            contr.addDiagnosticReport(repairOrderId, "Fix flat tire and other stuff");
+            System.out.println("Diagnostic report added to: " + repairOrderId);
+
+            contr.addRepairTask(repairOrderId, "Change all brake wires", 249.90);
+            System.out.println("Repair task added to: " + repairOrderId);
+
+            RepairOrderDTO repairOrder = contr.findRepairOrder(phoneNumber);
+            if(repairOrder != null){
+                System.out.println("Latest repair order: " + repairOrder.getRepairOrderId());
+                System.out.println("Status: " + repairOrder.getState());
+                System.out.println("Total cost: " + repairOrder.getTotalCost());
+            }
+            contr.acceptRepairOrder(repairOrderId);
+
         } catch (UserNotFoundException e) {
             System.out.println("ERROR: " + e.getMessage());
         } catch (DatabaseFailureException e) {
             System.out.println("ERROR: Something went wrong when trying to reach server. Please try again later.");
         }
-        
-        System.out.println("----- Sample Execution -----");
-
-        if (foundCustomer != null) {
-            System.out.println("Customer found: " + foundCustomer.getName());
-        } else {
-            System.out.println("No customer found on this number: " + phoneNumber);
-        }
-
-        contr.createRepairOrder("Flat tire and a broken front-light", phoneNumber, "BIKE-001");
-
-        String repairOrderId = "RO-1";
-        contr.addDiagnosticReport(repairOrderId, "Fix flat tire and other stuff");
-        System.out.println("Diagnostic report added to: " + repairOrderId);
-
-        contr.addRepairTask(repairOrderId, "Change all brake wires", 249.90);
-        System.out.println("Repair task added to: " + repairOrderId);
-
-        RepairOrderDTO repairOrder = contr.findRepairOrder(phoneNumber);
-        if(repairOrder != null){
-            System.out.println("Latest repair order: " + repairOrder.getRepairOrderId());
-            System.out.println("Status: " + repairOrder.getState());
-            System.out.println("Total cost: " + repairOrder.getTotalCost());
-        }
-        contr.acceptRepairOrder(repairOrderId);
     }
+        
+       
 
 }
