504ab410ae04ed7f42d2430f3b678cb05d071790
Gustaf Blomquist <glustone@proton.me>
2026-05-11 13:02:33 +0200

    Changed into Singleton

--- src/main/java/se/kth/iv1350/integration/CustomerRegistry.java
+++ src/main/java/se/kth/iv1350/integration/CustomerRegistry.java
@@ -5,23 +5,42 @@ import java.util.List;
 
 import se.kth.iv1350.integration.exceptions.UserNotFoundException;
 import se.kth.iv1350.integration.exceptions.DatabaseFailureException;
+import se.kth.iv1350.util.Logger;
 
 /**
  * Represents a customer registry and contains logic for searching for customers.
  */
 public class CustomerRegistry{
+
+    private static final CustomerRegistry INSTANCE = new CustomerRegistry();
+
+    private Logger logger;
+
     private List<CustomerDTO> customers = new ArrayList<>();
 
     /**
-     * Creates a new CustomerReigstry and initializes it with existing customers.
+     * Creates a new CustomerRegistry and initializes it with existing customers.
      */
-    public CustomerRegistry() {
+    private CustomerRegistry() {
         customers.add(new CustomerDTO("1234", "Gustaf"));
         customers.add(new CustomerDTO("5678", "Tova"));
     }
 
     /**
+     * Method for access to CustomerRegistry due to Singleton GoF.
+     * @return INSTANCE of CustomerRegistry.
+     */
+    public static CustomerRegistry getCustomerRegistry(){
+        return INSTANCE;
+    }
+
+    public void setLogger (Logger logger){
+        this.logger = logger;
+    }
+
+    /**
      * Retrieves information about an existing customer based on their phonenumber.
+     * To avoid NPE an if-statement is used for the logger call.
      * @param phoneNumber The customer's phone number.
      * @return Information about the customer in the form of a CustomerDTO.
      * @throws UserNotFoundException if no customer with the specified phone
@@ -30,6 +49,9 @@ public class CustomerRegistry{
      */
     public CustomerDTO findCustomer(String phoneNumber) throws UserNotFoundException, DatabaseFailureException {
         if (phoneNumber.equals("0000")) {
+            if (logger != null) {
+                logger.log("Database failure occured");
+            }
             throw new DatabaseFailureException("Could not reach database.");
         }
         for (CustomerDTO customer : customers) {
