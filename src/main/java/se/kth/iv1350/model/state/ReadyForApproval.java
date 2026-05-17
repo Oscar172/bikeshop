2579c1670e1c28de08af34d98088ca2a3d02b247
Oscar Jernemalm <jernemalm.oscar@gmail.com>
2026-05-17 16:41:21 +0200

    Added javac to state-classes and RepairOrder class

--- src/main/java/se/kth/iv1350/model/state/ReadyForApproval.java
+++ src/main/java/se/kth/iv1350/model/state/ReadyForApproval.java
@@ -4,28 +4,53 @@ import se.kth.iv1350.model.RepairOrder;
 
 public class ReadyForApproval implements RepairOrderState{
 
-    @Override
-    public void accept(RepairOrder order){
-        order.setState(new AcceptedState());
-    }
+      /**
+       * Handles an attempt to accept the repair order.
+       *
+       * @param order The repair order whose state may be changed.
+       */
+      @Override
+      public void accept(RepairOrder order){
+          order.setState(new AcceptedState());
+      }
 
-    @Override
-    public void reject(RepairOrder order){
-        order.setState(new RejectedState());
-    }
+      /**
+       * Handles an attempt to reject the repair order.
+       *
+       * @param order The repair order whose state may be changed.
+       */
+      @Override
+      public void reject(RepairOrder order){
+          order.setState(new RejectedState());
+      }
 
-    @Override
-    public void payed(RepairOrder order){
-        order.setState(new PayedState());
-    }
+      /**
+       * Handles an attempt to mark the repair order as paid.
+       *
+       * @param order The repair order whose state may be changed.
+       */
+      @Override
+      public void paid(RepairOrder order){
+          order.setState(new PaidState());
+      }
 
-    @Override
-    public void readyForApproval(RepairOrder order){
-        order.setState(new ReadyForApproval());
-    }
+      /**
+       * Handles an attempt to mark the repair order as ready for approval.
+       *
+       * @param order The repair order whose state may be changed.
+       */
+      @Override
+      public void readyForApproval(RepairOrder order){
+          //Already ready for approval, no state change is made.
+      }
 
-    @Override
-    public String getStateName(){
-        return "READYFORAPPROVAL";
-    }
-}
+      /**
+       * Returns the name of this state.
+       *
+       * @return The state name.
+       */
+      @Override
+      public String getStateName(){
+          return "READYFORAPPROVAL";
+      }
+  }
